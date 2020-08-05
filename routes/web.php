992617cf99b60b008acdf5f2849bed764897cfe9
Duong Nguyen <duongbgvip1@gmail.com>
2020-08-05 10:46:15 +0900

    fix dashboardsafe

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/dashboardtest', 'Admin\DashboardController@getSafeCheck');
 
 Route::group(['middleware' => ['auth', 'admin']], function () {
     Route::get('/dashboard', 'Admin\DashboardController@getSafeCheck');
