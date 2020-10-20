257e884bae28768933c17bf4b6959b0d32068836
Duong Nguyen <duongbgvip1@gmail.com>
2020-10-20 14:29:35 +0900

    delete db2

--- resources/views/admin/dashboard2.blade.php
+++ resources/views/admin/dashboard2.blade.php
@@ -10,28 +10,6 @@
 
 @section('content')
 
-<div class="row">
-  <div class="col-md-12">
-    <div class="card">
-      <div class="card-header">
-        <h4 class="card-title"> Registered Roles</h4>
-      </div>
-      <div class="card-body">
-        <div class="table-responsive">
-          
-          
-
-          
-
-          <a href="/dashboardsafe/"class="btn btn-success">Go</a>
-          
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-  
 
 @endsection
 
