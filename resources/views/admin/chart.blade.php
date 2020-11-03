00863c3966fdeed0f4578be45bb10d8abe859435
duongDiesel <duongbgvip2@gmail.com>
2020-11-03 23:04:24 +0900

    add print_f

--- resources/views/admin/chart.blade.php
+++ resources/views/admin/chart.blade.php
@@ -1,54 +1,25 @@
-<!DOCTYPE html>
-<html>
- <head>
-  <title>Make Google Pie Chart in Laravel</title>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-  <style type="text/css">
-   .box{
-    width:800px;
-    margin:0 auto;
-   }
-  </style>
-  <script type="text/javascript">
-   var analytics = <?php echo $gender; ?>
+@extends('layouts.app')
 
-   google.charts.load('current', {'packages':['corechart']});
+@section('content')
+<div >
+  <div >
+    <div >
+      <div >
+        <div >Chart Demo</div>
 
-   google.charts.setOnLoadCallback(drawChart);
+        <div >
+          {!! $chart->html() !!}
+        </div>
 
-   function drawChart()
-   {
-    var data = google.visualization.arrayToDataTable(analytics);
-    var options = {
-     title : 'Percentage of Male and Female Employee'
-    };
-    var chart = new google.visualization.PieChart(document.getElementById('pie_chart'));
-    chart.draw(data, options);
-   }
-  </script>
- </head>
- <body>
-  <br />
-  <div class="container">
-   <h3 align="center">Make Google Pie Chart in Laravel</h3><br />
-   
-   <div class="panel panel-default">
-    <div class="panel-heading">
-     <h3 class="panel-title">Percentage of Male and Female Employee</h3>
-    </div>
-    <div class="panel-body" align="center">
-     <div id="pie_chart" style="width:750px; height:450px;">
-
-     </div>
-    </div>
-   </div>
-   
-  </div>
-
-  <hr>
+        <hr>
         {!!$pie->html() !!}
-        {!! $pie->script() !!}
- </body>
-</html>
+      </div>
+    </div>
+  </div>
+</div>
+{!! Charts::scripts() !!}
+{!! $chart->script() !!}
+
+{!! $pie->script() !!}
+
+@endsection
