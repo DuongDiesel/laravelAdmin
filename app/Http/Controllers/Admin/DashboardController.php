13400ca3dc1688d94f13507ece42bd2c3f685581
duongDiesel <duongbgvip2@gmail.com>
2020-11-06 10:23:27 +0900

    try query

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -26,6 +26,7 @@ class DashboardController extends Controller
         $fields = Input::get('defaultExampleRadios');
         if($fields == 'safe'){
             $safecheck = DB::select('SELECT public.users_line.user_id, public.users_line.user_name, public.safe_check.line_id, public.safe_check.is_safe, public.safe_check.safe_location, public.safe_check.safe_mess, public.safe_check.time_update FROM public.safe_check,public.users_line WHERE public.users_line.line_userid = public.safe_check.line_id AND public.safe_check.is_safe = "safe"');
+            print_r($safecheck);
             return view('admin.dashboard')->with('safecheck',$safecheck); 
         }
         elseif ($fields == 'notsafe') {
