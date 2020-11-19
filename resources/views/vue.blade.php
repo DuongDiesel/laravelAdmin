6fe8be1e77ed255c5cc6cb49ec4c7b5048a0b8c7
duongDiesel <duongbgvip2@gmail.com>
2020-11-19 10:32:27 +0900

    edit vue compo

--- resources/views/vue.blade.php
+++ resources/views/vue.blade.php
@@ -12,7 +12,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background-color: #4545;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
