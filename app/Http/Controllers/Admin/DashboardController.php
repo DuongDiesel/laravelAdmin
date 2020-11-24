4fa19f711176a1188e4abe2cc0aeb8f35bcfcce6
duongDiesel <duongbgvip2@gmail.com>
2020-11-24 10:44:52 +0900

    fix dashboard2report

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\User;
 use DB;
+use Charts;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -49,8 +50,103 @@ class DashboardController extends Controller
    }
    public function dashboard2report($time = 1)
    {
-        $tempcheck = DB::select('SELECT public.users_line.user_id, public.users_line.user_name, public.temp_check.line_id, public.temp_check.temp, public.temp_check.temp_time FROM public.temp_check,public.users_line WHERE public.users_line.line_userid = public.temp_check.line_id ');
-        return view('report.dashboard2report')->with('tempcheck',$tempcheck);      
+        $times=1604298448104;
+        $A_respond = DB::select('SELECT DISTINCT
+ 
+        count(*) 
+         
+        FROM 
+         
+        public.users_line 
+         
+        LEFT JOIN 
+         
+        public.safe_check
+         
+        ON 
+         
+        public.users_line.line_userid = public.safe_check .line_id 
+         
+        WHERE 
+         
+        public.safe_check .line_id IS NULL');
+         
+        $A_respond = $A_respond[0]->count;
+
+
+        $All_respond= DB::select('SELECT 
+        count(*) 
+        FROM public.users_line');
+
+        $All_respond = $All_respond[0]->count;
+
+        // $b= ($All[0]->count) - ($a[0]->count);
+
+        $b_respond= $All_respond - $A_respond;
+        
+        $A_Safe = DB::select("SELECT
+         
+         count (*)
+
+         FROM public.safe_check
+         
+         WHERE 
+         
+         is_safe='Safe'
+         
+         and
+         
+         time_update >='$times'
+         
+         GROUP BY
+         
+          line_id");
+          
+         $A_Safe = $A_Safe[0]->count;
+ 
+ 
+        $B_Safe= DB::select("SELECT 
+        
+        count (*)
+
+         FROM public.safe_check
+         
+         WHERE 
+         
+         is_safe='Not Safe'
+         
+         and
+         
+         time_update >='$times'
+         
+         GROUP BY 
+         
+         line_id");
+ 
+         $B_Safe = $B_Safe[0]->count;
+
+        $pie_respond = Charts::create('pie', 'highcharts')
+         ->title('Answer Status')
+         ->labels(['Replied', "Didn't reply"])
+         ->values([$b_respond,$A_respond])
+         ->dimensions(530,350)
+         ->responsive(false);
+
+
+
+
+         $pie_Safe = Charts::create('pie', 'highcharts')
+         ->title('Safety Status')
+         ->labels(['Safe', 'Not Safe'])
+         ->values([$A_Safe,$B_Safe])
+         ->dimensions(490,350)
+         ->responsive(false);
+
+         $safecheck = DB::select('SELECT public.users_line.user_id, public.users_line.user_name, public.safe_check.line_id, public.safe_check.is_safe, public.safe_check.safe_location, public.safe_check.safe_mess, public.safe_check.time_update FROM public.safe_check,public.users_line WHERE public.users_line.line_userid = public.safe_check.line_id ');
+         
+        // return view('admin.chart')->with('pie_respond',$pie_respond);
+         return view('admin.dashboard2report',compact('pie_respond','pie_Safe','safecheck'));
+
         
    }
 }
