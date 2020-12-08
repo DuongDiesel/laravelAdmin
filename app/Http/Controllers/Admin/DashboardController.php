5e6592b1a64f469af9599ccf2d478cc48d5da131
duongDiesel <duongbgvip2@gmail.com>
2020-12-08 22:48:53 +0900

    add dd

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -173,7 +173,7 @@ class DashboardController extends Controller
 
          //dd($safecheck);
 
-         $notrep = B::select("SELECT public.users_line.user_id, public.users_line.user_name, public.safe_check.line_id, public.safe_check.is_safe, public.safe_check.safe_location, public.safe_check.safe_mess, public.safe_check.time_update 
+         $notrep = DB::select("SELECT public.users_line.user_id, public.users_line.user_name, public.safe_check.line_id, public.safe_check.is_safe, public.safe_check.safe_location, public.safe_check.safe_mess, public.safe_check.time_update 
          FROM public.safe_check,public.users_line 
 
          WHERE public.users_line.line_id NOT IN(SELECT public.safe_check.line_id FROM public.safe_check )
