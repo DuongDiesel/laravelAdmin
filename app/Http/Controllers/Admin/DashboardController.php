b834e7f4db2db2a76b344d9aff451dcb5f5fa2ea
duongDiesel <duongbgvip2@gmail.com>
2020-12-08 21:29:41 +0900

    add sql

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -154,7 +154,18 @@ class DashboardController extends Controller
          ->dimensions(490,350)
          ->responsive(false);
 
-         $safecheck = DB::select('SELECT public.users_line.user_id, public.users_line.user_name, public.safe_check.line_id, public.safe_check.is_safe, public.safe_check.safe_location, public.safe_check.safe_mess, public.safe_check.time_update FROM public.safe_check,public.users_line WHERE public.users_line.line_userid = public.safe_check.line_id ');
+         $safecheck = DB::select("SELECT public.users_line.user_id, public.users_line.user_name, public.safe_check.line_id, public.safe_check.is_safe, public.safe_check.safe_location, public.safe_check.safe_mess, public.safe_check.time_update 
+         FROM public.safe_check,public.users_line 
+
+         WHERE public.users_line.line_userid = public.safe_check.line_id 
+
+         and
+
+         public.safe_check.time_update >='$time_1'
+
+         and
+
+         public.safe_check.time_update <='$time_2'");
          
         // return view('admin.chart')->with('pie_respond',$pie_respond);
          return view('report.dashboard2report',compact('pie_respond','pie_Safe','safecheck'));
