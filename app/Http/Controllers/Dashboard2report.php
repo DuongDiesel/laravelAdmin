1d04b78b24a8d3d2a87ed621dea0e0cc9b68ecaa
duongDiesel <duongbgvip2@gmail.com>
2020-12-09 11:03:48 +0900

    add dd

--- app/Http/Controllers/Dashboard2report.php
+++ app/Http/Controllers/Dashboard2report.php
@@ -158,8 +158,11 @@ class Dashboard2report extends Controller
         info($time);
         $time_1=$time;
         $time_2=$time+86399998;
+        print_r($time_1);
+        print_r($time_2);
 
         $safecheck = DB::select("SELECT public.users_line.user_id, public.users_line.user_name, public.safe_check.line_id, public.safe_check.is_safe, public.safe_check.safe_location, public.safe_check.safe_mess, public.safe_check.time_update 
+
         FROM public.safe_check,public.users_line 
 
         WHERE public.users_line.line_userid = public.safe_check.line_id 
@@ -177,6 +180,27 @@ class Dashboard2report extends Controller
         public.safe_check.is_safe != 'Safe'");
 
         dd($safecheck);
+
+        $safecheck2 = DB::select("SELECT public.users_line.user_id, public.users_line.user_name, public.safe_check.line_id, public.safe_check.is_safe, public.safe_check.safe_location, public.safe_check.safe_mess, public.safe_check.time_update 
+
+        FROM public.safe_check,public.users_line 
+
+        WHERE public.users_line.line_userid = public.safe_check.line_id 
+
+        AND
+
+        public.safe_check.time_update >='$time_1'
+
+        AND
+
+        public.safe_check.time_update <='$time_2'
+        
+        AND 
+
+        public.safe_check.is_safe != 'Safe'
+            
+        ");
+
     }
  
 }
