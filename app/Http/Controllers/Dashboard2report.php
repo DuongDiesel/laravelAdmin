3481c70cec797b3756c1bbcd84f0b7a329aa8ccb
duongDiesel <duongbgvip2@gmail.com>
2020-12-10 00:58:00 +0900

    add dd

--- app/Http/Controllers/Dashboard2report.php
+++ app/Http/Controllers/Dashboard2report.php
@@ -51,10 +51,9 @@ class Dashboard2report extends Controller
 
         //dd($safecheck3);
 
-        $safecheck4 = DB::select("SELECT public.users_line.user_id, public.users_line.user_name, public.safe_check.line_id, public.safe_check.is_safe, public.safe_check.safe_location, public.safe_check.safe_mess, public.safe_check.time_update 
-        FROM public.safe_check,public.users_line 
-        WHERE public.users_line.line_userid = public.safe_check.line_id 
-        AND public.safe_check.time_update >='$time_1'
+        $safecheck4 = DB::select("SELECT public.safe_check.line_id 
+        FROM public.safe_check
+        WHERE public.safe_check.time_update >='$time_1'
         AND public.safe_check.time_update <='$time_2'");
 
         dd($safecheck4);
