9058151c1080297a459c616f7bb67d95ccfbfefc
duongDiesel <duongbgvip2@gmail.com>
2020-12-09 10:35:06 +0900

    add dd

--- app/Http/Controllers/Dashboard2report.php
+++ app/Http/Controllers/Dashboard2report.php
@@ -152,5 +152,31 @@ class Dashboard2report extends Controller
  
          
     }
+
+    public function test($time){
+        print_r($time);
+        info($time);
+        $time_1=$time;
+        $time_2=$time+86399998;
+
+        $safecheck = DB::select("SELECT public.users_line.user_id, public.users_line.user_name, public.safe_check.line_id, public.safe_check.is_safe, public.safe_check.safe_location, public.safe_check.safe_mess, public.safe_check.time_update 
+        FROM public.safe_check,public.users_line 
+
+        WHERE public.users_line.line_userid = public.safe_check.line_id 
+
+        and
+
+        public.safe_check.time_update >='$time_1'
+
+        and
+
+        public.safe_check.time_update <='$time_2'
+        
+        and 
+
+        public.safe_check.is_safe != 'Safe'");
+
+        dd($safecheck);
+    }
  
 }
