4e05f8c437d955eb005424e67b0a6f95fb79807a
duongDiesel <duongbgvip2@gmail.com>
2020-12-09 11:13:59 +0900

    add dd

--- app/Http/Controllers/Dashboard2report.php
+++ app/Http/Controllers/Dashboard2report.php
@@ -181,25 +181,18 @@ class Dashboard2report extends Controller
 
         dd($safecheck);
 
-        $safecheck2 = DB::select("SELECT public.users_line.user_id, public.users_line.user_name, public.safe_check.line_id, public.safe_check.is_safe, public.safe_check.safe_location, public.safe_check.safe_mess, public.safe_check.time_update 
-
-        FROM public.safe_check,public.users_line 
-
-        WHERE public.users_line.line_userid = public.safe_check.line_id 
-
-        AND
-
-        public.safe_check.time_update >='$time_1'
-
-        AND
-
-        public.safe_check.time_update <='$time_2'
-        
-        AND 
-
-        public.safe_check.is_safe != 'Safe'
+        $safecheck2 = DB::select("SELECT DISTINCT ON (public.safe_check.line_id) public.safe_check.line_id, public.safe_check.time_update
+        FROM public.safe_check 
+        WHERE line_id IN (SELECT DISTINCT public.safe_check.line_id FROM public.safe_check)
+        AND public.safe_check.time_update >='$time_1'
+        AND public.safe_check.time_update <='$time_2'
+        AND public.safe_check.is_safe != 'Safe'
+        ORDER BY public.safe_check.line_id, id desc;
             
         ");
+
+        dd($safecheck);
+
 
     }
  
