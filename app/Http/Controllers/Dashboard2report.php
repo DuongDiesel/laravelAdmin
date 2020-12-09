3768df9665ae51341fa9da2f060488cd8f409c7c
duongDiesel <duongbgvip2@gmail.com>
2020-12-09 11:30:34 +0900

    add dd

--- app/Http/Controllers/Dashboard2report.php
+++ app/Http/Controllers/Dashboard2report.php
@@ -10,148 +10,7 @@ use App\Http\Controllers\Controller;
 
 class Dashboard2report extends Controller
 {
-    public function dashboard2report($time = 1)
-    {    
-         print_r($time);
-         info($time);
-         $time_1=$time;
-         $time_2=$time+86399998;
-         //$times=1604298448104;
-         $A_respond = DB::select('SELECT DISTINCT
-  
-         count(*) 
-          
-         FROM 
-          
-         public.users_line 
-          
-         LEFT JOIN 
-          
-         public.safe_check
-          
-         ON 
-          
-         public.users_line.line_userid = public.safe_check .line_id 
-          
-         WHERE 
-          
-         public.safe_check .line_id IS NULL');
-          
-         $A_respond = $A_respond[0]->count;
- 
- 
-         $All_respond= DB::select('SELECT 
-         count(*) 
-         FROM public.users_line');
- 
-         $All_respond = $All_respond[0]->count;
- 
-         // $b= ($All[0]->count) - ($a[0]->count);
- 
-         $b_respond= $All_respond - $A_respond;
-         
-         $A_Safe = DB::select("SELECT
-          
-          count (*)
- 
-          FROM public.safe_check
-          
-          WHERE 
-          
-          is_safe='Safe'
-          
-          and
-          
-          time_update >='$time_1'
- 
-          and
- 
-          time_update <='$time_2'
-          
-          GROUP BY
-          
-           line_id");
-           
-          $A_Safe = $A_Safe[0]->count;
-  
-  
-         $B_Safe= DB::select("SELECT 
-         
-         count (*)
- 
-          FROM public.safe_check
-          
-          WHERE 
-          
-          is_safe='Not Safe'
-          
-          and
-          
-          time_update >='$time_1'
- 
-          and
- 
-          time_update <='$time_2'
-          
-          GROUP BY 
-          
-          line_id");
-  
-          $B_Safe = $B_Safe[0]->count;
- 
-         $pie_respond = Charts::create('pie', 'highcharts')
-          ->title('Answer Status')
-          ->labels(['Replied', "Didn't reply"])
-          ->values([$b_respond,$A_respond])
-          ->dimensions(530,350)
-          ->responsive(false);
- 
- 
- 
- 
-          $pie_Safe = Charts::create('pie', 'highcharts')
-          ->title('Safety Status')
-          ->labels(['Safe', 'Not Safe'])
-          ->values([$A_Safe,$B_Safe])
-          ->dimensions(490,350)
-          ->responsive(false);
- 
-          $safecheck = DB::select("SELECT public.users_line.user_id, public.users_line.user_name, public.safe_check.line_id, public.safe_check.is_safe, public.safe_check.safe_location, public.safe_check.safe_mess, public.safe_check.time_update 
-          FROM public.safe_check,public.users_line 
- 
-          WHERE public.users_line.line_userid = public.safe_check.line_id 
- 
-          and
- 
-          public.safe_check.time_update >='$time_1'
- 
-          and
- 
-          public.safe_check.time_update <='$time_2'
-          
-          and 
- 
-          public.safe_check.is_safe != 'Safe'");
- 
-          dd($safecheck);
- 
-          
-          $notrep = DB::select("SELECT public.users_line.user_id, public.users_line.line_userid, public.users_line.time_update
- 
-          FROM  public.users_line
-          
-          WHERE public.users_line.line_userid NOT IN(SELECT public.safe_check.line_id FROM public.safe_check WHERE public.safe_check.time_update >= '$time_1'AND public.safe_check.time_update <= '$time_2')");
- 
-          //dd($notrep);
- 
- 
-          
-         // return view('admin.chart')->with('pie_respond',$pie_respond);
-         // return view('report.dashboard2report',compact('pie_respond','pie_Safe','safecheck'));
-          return view('report.dashboard2test');
- 
-         
-    }
+    
 
     public function test($time){
         print_r($time);
@@ -162,26 +21,15 @@ class Dashboard2report extends Controller
         print_r($time_2);
 
         $safecheck = DB::select("SELECT public.users_line.user_id, public.users_line.user_name, public.safe_check.line_id, public.safe_check.is_safe, public.safe_check.safe_location, public.safe_check.safe_mess, public.safe_check.time_update 
-
         FROM public.safe_check,public.users_line 
-
         WHERE public.users_line.line_userid = public.safe_check.line_id 
-
-        and
-
-        public.safe_check.time_update >='$time_1'
-
-        and
-
-        public.safe_check.time_update <='$time_2'
-        
-        and 
-
-        public.safe_check.is_safe != 'Safe'");
+        AND public.safe_check.time_update >='$time_1'
+        AND public.safe_check.time_update <='$time_2'
+        AND public.safe_check.is_safe != 'Safe'");
 
         //dd($safecheck);
 
-        $safecheck2 = DB::select("SELECT DISTINCT ON (public.safe_check.line_id) public.safe_check.line_id, public.safe_check.time_update
+        $safecheck2 = DB::select("SELECT DISTINCT ON (public.safe_check.line_id) public.safe_check.line_id, public.safe_check.is_safe, public.safe_check.time_update
         FROM public.safe_check 
         WHERE line_id IN (SELECT DISTINCT public.safe_check.line_id FROM public.safe_check)
         AND public.safe_check.time_update >='$time_1'
@@ -193,6 +41,7 @@ class Dashboard2report extends Controller
 
         dd($safecheck2);
 
+        return view('report.dashboard2test',compact('safecheck','safecheck2'));
 
     }
  
--- resources/views/report/dashboard2test.blade.php
+++ resources/views/report/dashboard2test.blade.php
@@ -11,7 +11,71 @@
 @section('content')
 
   
-   
+<div class="col-9">
+    <div class="card">
+      <div class="card-header">
+        <h4 class="card-title">safecheck</h4>
+      </div>
+      <div class="card-body">
+        <div class="table-responsive">
+          <table class="table">
+            <thead class=" text-primary">
+              <th>user_id</th>
+              <th>user_name</th>
+              <th>line_id</th>
+              <th>is_safe</th>
+              <th>safe_location</th> 
+              <th>safe_mess</th> 
+              <th>time_update</th>                    
+            </thead>
+            <tbody>                
+              @foreach ($safecheck as $rowsafecheck)
+                <tr>
+                <td>{{ $rowsafecheck->user_id }}</td>
+                <td>{{ $rowsafecheck->user_name }}</td>
+                <td>{{ $rowsafecheck->line_id }}</td>
+                <td>{{ $rowsafecheck->is_safe }}</td>
+                <td>{{ $rowsafecheck->safe_location }}</td>
+                <td>{{ $rowsafecheck->safe_mess }}</td> 
+                <td>{{ $rowsafecheck->time_update }}</td>                    
+                </tr>
+              @endforeach                   
+              
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-9">
+    <div class="card">
+      <div class="card-header">
+        <h4 class="card-title">safecheck2</h4>
+      </div>
+      <div class="card-body">
+        <div class="table-responsive">
+          <table class="table">
+            <thead class=" text-primary">
+              <th>line_id</th>
+              <th>is_safe</th>
+              <th>time_update</th>                    
+            </thead>
+            <tbody>                
+              @foreach ($safecheck2 as $rowsafecheck2)
+                <tr>
+                <td>{{ $rowsafecheck->line_id }}</td>
+                <td>{{ $rowsafecheck->is_safe }}</td>
+                <td>{{ $rowsafecheck->time_update }}</td>                    
+                </tr>
+              @endforeach                   
+              
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
   
 
 @endsection
