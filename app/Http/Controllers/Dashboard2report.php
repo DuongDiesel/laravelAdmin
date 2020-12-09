c5a99db145756d82004f003c9e79c1ab29454e83
duongDiesel <duongbgvip2@gmail.com>
2020-12-09 23:58:11 +0900

    add dd

--- app/Http/Controllers/Dashboard2report.php
+++ app/Http/Controllers/Dashboard2report.php
@@ -17,7 +17,7 @@ class Dashboard2report extends Controller
         info($time);
         $time_1=$time;
         $time_2=$time+86399998;
-        print_r($time_1);
+        //print_r($time_1);
         print_r($time_2);
 
         $safecheck = DB::select("SELECT public.users_line.user_id, public.users_line.user_name, public.safe_check.line_id, public.safe_check.is_safe, public.safe_check.safe_location, public.safe_check.safe_mess, public.safe_check.time_update 
@@ -34,12 +34,11 @@ class Dashboard2report extends Controller
         WHERE line_id IN (SELECT DISTINCT public.safe_check.line_id FROM public.safe_check)
         AND public.safe_check.time_update >='$time_1'
         AND public.safe_check.time_update <='$time_2'
-        
         ORDER BY public.safe_check.line_id, id desc;
             
         ");
 
-        dd($safecheck2);
+        //dd($safecheck2);
 
         $safecheck3 = DB::select("SELECT tav.line_id, tav.is_safe, tav.time_update
         FROM(SELECT DISTINCT ON (public.safe_check.line_id) public.safe_check.line_id, public.safe_check.is_safe,public.safe_check.time_update
@@ -47,9 +46,7 @@ class Dashboard2report extends Controller
                 WHERE line_id IN (SELECT DISTINCT public.safe_check.line_id FROM public.safe_check)
                 AND public.safe_check.time_update >='$time_1'
                 AND public.safe_check.time_update <='$time_2'
-        
                 ORDER BY public.safe_check.line_id, id desc) AS tav
-        
         WHERE tav.is_safe != 'Safe'");
 
         //dd($safecheck3);
