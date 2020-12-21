216bf56fa08dd85b9d98e59f8dbdf8b1e6f2814c
duongDiesel <duongbgvip2@gmail.com>
2020-12-21 21:41:41 +0900

    dd safecheck2

--- app/Http/Controllers/Dashboardreport.php
+++ app/Http/Controllers/Dashboardreport.php
@@ -11,8 +11,8 @@ use App\Http\Controllers\Controller;
 class Dashboardreport extends Controller
 {
     
-
     public function test($time){
+
         print_r($time);
         info($time);
         $time_1=$time;
@@ -20,6 +20,9 @@ class Dashboardreport extends Controller
         //print_r($time_1);
         print_r($time_2);
 
+        
+
+        //lated submit -ko dung cai nay
         $safecheck = DB::select("SELECT public.users_line.user_id, public.users_line.user_name, public.safe_check.line_id, public.safe_check.is_safe, public.safe_check.safe_location, public.safe_check.safe_mess, public.safe_check.time_update 
         FROM public.safe_check,public.users_line 
         WHERE public.users_line.line_userid = public.safe_check.line_id 
@@ -28,18 +31,17 @@ class Dashboardreport extends Controller
         AND public.safe_check.is_safe != 'Safe'");
 
         //dd($safecheck);
-
+        //lated submit per user-dung cai nay-day la nhung ng da su dung vao ngay hom day
         $safecheck2 = DB::select("SELECT DISTINCT ON (public.safe_check.line_id) public.safe_check.line_id, public.safe_check.is_safe, public.safe_check.time_update
         FROM public.safe_check 
         WHERE line_id IN (SELECT DISTINCT public.safe_check.line_id FROM public.safe_check)
         AND public.safe_check.time_update >='$time_1'
         AND public.safe_check.time_update <='$time_2'
         ORDER BY public.safe_check.line_id, id desc;
-            
         ");
 
-        //dd($safecheck2);
-
+        dd($safecheck2[0]->count);
+        // dung cai nay de hien ra nhung ng da nhap vao ngay hom day va not safe
         $safecheck3 = DB::select("SELECT tav.line_id, tav.is_safe, tav.time_update
         FROM(SELECT DISTINCT ON (public.safe_check.line_id) public.safe_check.line_id, public.safe_check.is_safe,public.safe_check.time_update
             FROM public.safe_check 
@@ -66,6 +68,25 @@ class Dashboardreport extends Controller
         AND public.safe_check.time_update <='$time_2')
         
         ");
+
+        $b_respond=3;
+        $A_respond=3;
+        $A_Safe=3;
+        $B_Safe=3;
+
+        $pie_respond = Charts::create('pie', 'highcharts')
+        ->title('Answer Status')
+        ->labels(['Replied', "Didn't reply"])
+        ->values([$b_respond,$A_respond])
+        ->dimensions(530,350)
+        ->responsive(false);
+
+        $pie_Safe = Charts::create('pie', 'highcharts')
+        ->title('Safety Status')
+        ->labels(['Safe', 'Not Safe'])
+        ->values([$A_Safe,$B_Safe])
+        ->dimensions(490,350)
+        ->responsive(false);
 
         //dd($safecheck5);
 
--- resources/views/report/dashboardreport.blade.php
+++ resources/views/report/dashboardreport.blade.php
@@ -10,102 +10,125 @@
 
 @section('content')
 
+<!-- start container-->
+<div class="container-fluid" style="margin-top:50px;">
+  <!-- start panel-->
+   <div class="panel panel-default">
+     <div class="panel-heading">
+         <h3 style="text-align:center;" class="panel-title">Safety Check Results</h3>
+     </div>
 
-    <div class="card">
-      <div class="card-header">
-        <h4 class="card-title">safecheck</h4>
-      </div>
-      <div class="card-body">
-        <div class="table-responsive">
-          <table class="table">
-            <thead class=" text-primary">
-              <th>user_id</th>
-              <th>user_name</th>
-              <th>line_id</th>
-              <th>is_safe</th>
-              <th>safe_location</th> 
-              <th>safe_mess</th> 
-              <th>time_update</th>                    
-            </thead>
-            <tbody>                
-              @foreach ($safecheck as $rowsafecheck)
-                <tr>
-                <td>{{ $rowsafecheck->user_id }}</td>
-                <td>{{ $rowsafecheck->user_name }}</td>
-                <td>{{ $rowsafecheck->line_id }}</td>
-                <td>{{ $rowsafecheck->is_safe }}</td>
-                <td>{{ $rowsafecheck->safe_location }}</td>
-                <td>{{ $rowsafecheck->safe_mess }}</td> 
-                <td>{{ $rowsafecheck->time_update }}</td>
-                </tr>
-              @endforeach                   
-              
-            </tbody>
-          </table>
-        </div>
+     <!-- start chart row-->
+     <div class="row">
+       <div id="col1" class="col-lg-6 col-md-6 col-sm-12">
+         <div class="panel-body">
+           {!!$pie_Safe->html() !!}
+         </div>
+       </div>
+
+       <div id="col2" class="col-lg-6 col-md-6 col-sm-12">
+         <div class="panel-body">
+           {!!$pie_respond->html() !!}
+         </div>
+       </div>
+     </div>
+     <!-- end row-->
+   </div>
+   <!-- end panel -->
+
+ </div>
+ <!-- end row -->
+
+  <div class="card">
+    <div class="card-header">
+      <h4 class="card-title">safecheck</h4>
+    </div>
+    <div class="card-body">
+      <div class="table-responsive">
+        <table class="table">
+          <thead class=" text-primary">
+            <th>user_id</th>
+            <th>user_name</th>
+            <th>line_id</th>
+            <th>is_safe</th>
+            <th>safe_location</th> 
+            <th>safe_mess</th> 
+            <th>time_update</th>                    
+          </thead>
+          <tbody>                
+            @foreach ($safecheck as $rowsafecheck)
+              <tr>
+              <td>{{ $rowsafecheck->user_id }}</td>
+              <td>{{ $rowsafecheck->user_name }}</td>
+              <td>{{ $rowsafecheck->line_id }}</td>
+              <td>{{ $rowsafecheck->is_safe }}</td>
+              <td>{{ $rowsafecheck->safe_location }}</td>
+              <td>{{ $rowsafecheck->safe_mess }}</td> 
+              <td>{{ $rowsafecheck->time_update }}</td>
+              </tr>
+            @endforeach                   
+            
+          </tbody>
+        </table>
       </div>
     </div>
+  </div>
 
-
-    <div class="card">
-      <div class="card-header">
-        <h4 class="card-title">safecheck2-lated submit per user</h4>
-      </div>
-      <div class="card-body">
-        <div class="table-responsive">
-          <table class="table">
-            <thead class=" text-primary">
-              <th>line_id</th>
-              <th>is_safe</th>
-              <th>time_update</th>                    
-            </thead>
-            <tbody>                
-              @foreach ($safecheck2 as $rowsafecheck2)
-                <tr>
-                <td>{{ $rowsafecheck2->line_id }}</td>
-                <td>{{ $rowsafecheck2->is_safe }}</td>
-                <td>{{ $rowsafecheck2->time_update }}</td>                    
-                </tr>
-              @endforeach                   
-              
-            </tbody>
-          </table>
-        </div>
+  <div class="card">
+    <div class="card-header">
+      <h4 class="card-title">safecheck2-lated submit per user</h4>
+    </div>
+    <div class="card-body">
+      <div class="table-responsive">
+        <table class="table">
+          <thead class=" text-primary">
+            <th>line_id</th>
+            <th>is_safe</th>
+            <th>time_update</th>                    
+          </thead>
+          <tbody>                
+            @foreach ($safecheck2 as $rowsafecheck2)
+              <tr>
+              <td>{{ $rowsafecheck2->line_id }}</td>
+              <td>{{ $rowsafecheck2->is_safe }}</td>
+              <td>{{ $rowsafecheck2->time_update }}</td>                    
+              </tr>
+            @endforeach                   
+            
+          </tbody>
+        </table>
       </div>
     </div>
+  </div>
 
-
-    <div class="card">
-      <div class="card-header">
-        <h4 class="card-title">safecheck3-lated submit per user and not safe</h4>
-      </div>
-      <div class="card-body">
-        <div class="table-responsive">
-          <table class="table">
-            <thead class=" text-primary">
-              <th>line_id</th>
-              <th>is_safe</th>
-              <th>time_update</th>                    
-            </thead>
-            <tbody>                
-              @foreach ($safecheck3 as $rowsafecheck3)
-                <tr>
-                <td>{{ $rowsafecheck3->line_id }}</td>
-                <td>{{ $rowsafecheck3->is_safe }}</td>
-                <td>{{ $rowsafecheck3->time_update }}</td>                    
-                </tr>
-              @endforeach                   
-              
-            </tbody>
-          </table>
-        </div>
+  <div class="card">
+    <div class="card-header">
+      <h4 class="card-title">safecheck3-lated submit per user and not safe</h4>
+    </div>
+    <div class="card-body">
+      <div class="table-responsive">
+        <table class="table">
+          <thead class=" text-primary">
+            <th>line_id</th>
+            <th>is_safe</th>
+            <th>time_update</th>                    
+          </thead>
+          <tbody>                
+            @foreach ($safecheck3 as $rowsafecheck3)
+              <tr>
+              <td>{{ $rowsafecheck3->line_id }}</td>
+              <td>{{ $rowsafecheck3->is_safe }}</td>
+              <td>{{ $rowsafecheck3->time_update }}</td>                    
+              </tr>
+            @endforeach                   
+            
+          </tbody>
+        </table>
       </div>
     </div>
+  </div>
 
-  
-  
-
-@endsection
+    @endsection
 
 @section('scripts')
 <script type="text/javascript" src="js/app.js"></script>
