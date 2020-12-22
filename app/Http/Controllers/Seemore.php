43993fc7376ac33fafa24a2c498606fce3466777
duongDiesel <duongbgvip2@gmail.com>
2020-12-22 12:02:20 +0900

    add seemore

--- app/Http/Controllers/Seemore.php
+++ app/Http/Controllers/Seemore.php
@@ -9,9 +9,11 @@ class Seemore extends Controller
     //
     public function index($id){
 
-        $safecheck3 = DB::select("SELECT (*)
+        $id_1=$id;
+
+        $safecheck3 = DB::select("SELECT public.safe_check.line_id, public.safe_check.is_safe
         FROM public.safe_check
-        WHERE public.safe_check.id = '$id'
+        WHERE public.safe_check.id = '$id_1'
         ");
 
         dd($safecheck3);
