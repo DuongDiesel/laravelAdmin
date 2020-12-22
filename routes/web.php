03cba3aebbfec277cb764df207f47856d16dc934
duongDiesel <duongbgvip2@gmail.com>
2020-12-22 11:54:47 +0900

    add seemore

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ Route::group(['middleware' => ['auth', 'admin']], function () {
     Route::get('/dashboard1report/{time?}','Dashboard1report@test');
     Route::get('/dashboard2report/{time?}','Dashboard2report@test');
 
+    Route::resource('seemore','Seemore');
    
 
 });
