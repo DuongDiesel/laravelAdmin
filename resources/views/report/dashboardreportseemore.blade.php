fd828c9db7c1a2a5c3d0fd5d4b3595b8ed851751
duongDiesel <duongbgvip2@gmail.com>
2020-12-22 13:48:40 +0900

    add seemore

--- resources/views/report/dashboardreportseemore.blade.php
+++ resources/views/report/dashboardreportseemore.blade.php
@@ -10,7 +10,45 @@
 
 @section('content')
 
+<!-- start table 1 row-->
+<div class="row">
+    <div class="card">
+      <div class="card-header">
+        <h4 class="card-title">safecheck3-lated submit per user and not safe</h4>
+      </div>
+      <div class="card-body">
+        @if($safecheck3 == null)
+          <h3>No Data Was Found </h3>
+              
+        @else  
 
+        <div class="table-responsive">
+          <table class="table">
+            <thead class=" text-primary">
+              <th>id</th>
+              <th>line_id</th>
+              <th>is_safe</th>
+              <th>time_update</th>   
+              <th>choice</th>                  
+            </thead>
+            <tbody>                
+              @foreach ($safecheck3 as $rowsafecheck3)
+                <tr>
+                    
+                <td>{{ $rowsafecheck3->line_id }}</td>
+                <td>{{ $rowsafecheck3->is_safe }}</td>
+                                 
+                </tr>
+              @endforeach                   
+          @endif     
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end table row 1 -->
+  
 
  
 @endsection
