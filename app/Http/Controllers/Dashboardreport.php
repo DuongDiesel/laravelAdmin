7d68211ae032a3189981d447f61902053911924b
duongDiesel <duongbgvip2@gmail.com>
2020-12-24 11:08:04 +0900

    fix seemore

--- app/Http/Controllers/Dashboardreport.php
+++ app/Http/Controllers/Dashboardreport.php
@@ -32,7 +32,7 @@ class Dashboardreport extends Controller
 
         //----------------------------------------------------------------
         //lated submit per user-dung cai nay-day la nhung ng da su dung vao ngay hom day
-        $safecheck2 = DB::select("SELECT DISTINCT ON (public.safe_check.line_id) public.safe_check.line_id, public.safe_check.is_safe, public.safe_check.time_update
+        $safecheck2 = DB::select("SELECT DISTINCT ON (public.safe_check.line_id) public.safe_check.id, public.safe_check.line_id, public.safe_check.is_safe, public.safe_check.time_update
         FROM public.safe_check 
         WHERE line_id IN (SELECT DISTINCT public.safe_check.line_id FROM public.safe_check)
         AND public.safe_check.time_update >='$time_1'
