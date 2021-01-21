f01cd37516bf1ff49a7bd77849a8d5e5d6f38a5c
duongDiesel <duongbgvip2@gmail.com>
2021-01-21 15:33:56 +0900

    add map

--- resources/js/app.js
+++ resources/js/app.js
@@ -24,6 +24,13 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 Vue.component('dateclick', require('./components/DateClick.vue').default);
 Vue.component('dateclick1', require('./components/DateClick1.vue').default);
 
+import * as VueGoogleMaps from 'vue2-google-maps';
+
+Vue.use(VueGoogleMaps,{
+    load:{
+        
+    }
+})
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
--- routes/web.php
+++ routes/web.php
@@ -38,15 +38,16 @@ Route::group(['middleware' => ['auth', 'admin']], function () {
 
     //map
     Route::get('/map', function(){
-        $config = array();
-        $config['center'] = 'Defence Garden, Karachi';
-        $config['zoom'] = '14';
-        $config['map_height'] = '500px';
-        $config['scrollwheel'] = false;
-        GMaps::initialize($config);
-        $map = GMaps::create_map();
+        // $config = array();
+        // $config['center'] = 'Defence Garden, Karachi';
+        // $config['zoom'] = '14';
+        // $config['map_height'] = '500px';
+        // $config['scrollwheel'] = false;
+        // GMaps::initialize($config);
+        // $map = GMaps::create_map();
     
-        return view('map')->with('map', $map);
+        // return view('map')->with('map', $map);
+        return view('map');
     });
 
 
